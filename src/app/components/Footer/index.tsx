1a0c75a4e998b8c7b73792fb3e9e85205b5682fd
Felipe Augusto <augusto.felipedev@gmail.com>
2026-02-24 17:51:18 -0300

    feat: create Header and Footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+export default function Footer() {
+  return (
+    <div>
+      <p>Desenvolvido por:</p>
+      <span>Felipe Augusto</span>
+      <GitHubIcon />
+      <LinkedInIcon />
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Next-Blog",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
