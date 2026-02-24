774aecc1ea1e0450591711aa97cc93ef5e6d60d1
Felipe Augusto <augusto.felipedev@gmail.com>
2026-02-24 15:25:24 -0300

    feat: Create data list with Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import artigos from "@/data/artigos.json";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hey Next</h1>
+      {artigos.map((artigo) => (
+        <Link href={`/artigos/${artigo.slug}`} key={artigo.slug}>
+          <div>
+            <h2>{artigo.titulo}</h2>
+            <span>{artigo.autor}</span>
+            <data>{artigo.dataPublicacao}</data>
+            <p>{artigo.descricao}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
